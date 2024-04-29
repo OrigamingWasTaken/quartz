f3f959a20bbbb8a5f7becdf6641b3a9094078274
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-04-29 22:06:08 +0200

    Removed wintersmith .gitignore

---
+++ quartz/plugins/transformers/password.ts
@@ -0,0 +1,38 @@
+import { BuildCtx } from "../../util/ctx"
+import { Root as HTMLRoot } from "hast"
+import { QuartzTransformerPlugin } from "../types"
+import { encryptHTML } from "pagecrypt/core"
+import { toString } from "hast-util-to-string"
+import { VFile } from "vfile"
+import { escapeHTML } from "../../util/escape"
+import { fromHtml } from "hast-util-from-html"
+
+interface Options {
+  /*** Number of time to encrypt the page with the password (default: 3e6) */
+  iterations?: number,
+  /*** The description of every password-protected notes */
+  lockedDescription?: string,
+}
+
+export const PasswordProtection: QuartzTransformerPlugin = (opts?: Options) => {
+  return {
+    name: "PasswordProtection",
+    htmlPlugins() {
+      return [
+        () => {
+          return async (tree: HTMLRoot,file: VFile) => {
+            const frontmatter = file.data.frontmatter
+
+            let encryptedHTML = escapeHTML(toString(tree))
+            if (frontmatter && "password" in frontmatter && frontmatter.password) {
+              encryptedHTML = await encryptHTML(encryptedHTML,frontmatter.password as string, opts?.iterations || 3e6)
+            }
+            file.data.description = opts?.lockedDescription || "This note is password-protected."
+            return fromHtml(encryptedHTML)
+            // file.data.htmlAst = fromHtml(encryptedHTML)
+          }
+        }
+      ]
+    }
+  }
+}
