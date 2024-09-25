9e7b7f7e0add612aab09d7d0ba58ae6da4207950
OrigamingWasTaken <74014262+OrigamingWasTaken@users.noreply.github.com>
2024-09-25 17:48:15 +0200

    Change email

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,7 +8,7 @@ export const sharedPageComponents: SharedLayout = {
 	footer: Component.Footer({
 		links: {
 			GitHub: 'https://github.com/OrigamingWasTaken/quartz',
-			'Me contacter': 'mailto:contact@marlonb.ch',
+			'Me contacter': 'mailto:marlon@origaming.ch',
 		},
 	}),
 };
